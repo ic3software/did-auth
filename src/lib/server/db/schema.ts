f2205dd9c868028875b92b2dac70fdb79cabc112
vthwang <vincent@fishboneapps.com>
2025-03-12 16:10:32 -0700

    refactor: add created_at and updated_at
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,24 +1,42 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
-import { relations } from 'drizzle-orm';
+import { relations, sql } from 'drizzle-orm';
 
 export const users = sqliteTable('users', {
     id: integer('id').primaryKey({ autoIncrement: true }),
     name: text('name').unique().notNull(),
+    createdAt: integer('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: integer('updated_at').notNull().default(sql`CURRENT_TIMESTAMP`),
 });
 
 export const publicKeys = sqliteTable('public_keys', {
     id: integer('id').primaryKey({ autoIncrement: true }),
     userId: integer('user_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
     publicKey: text('public_key').unique().notNull(),
+    createdAt: integer('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const emails = sqliteTable('emails', {
+    id: integer('id').primaryKey({ autoIncrement: true }),
+    userId: integer('user_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
+    email: text('email').unique().notNull(),
+    createdAt: integer('created_at').notNull().default(sql`CURRENT_TIMESTAMP`),
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
     publicKeys: many(publicKeys),
+    emails: many(emails),
 }));
 
 export const publicKeysRelations = relations(publicKeys, ({ one }) => ({
     user: one(users, {
         fields: [publicKeys.userId],
+        references: [users.id],
+    }),
+}));
+
+export const emailsRelations = relations(emails, ({ one }) => ({
+    user: one(users, {
+        fields: [emails.userId],
         references: [users.id],
     }),
 }));
